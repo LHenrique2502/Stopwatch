5657a4baa3a1b7373889eb0568a2d845f66d2c4b
Lucas Henrique <120137183+LHenrique2502@users.noreply.github.com>
2024-02-28 20:51:44 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading;
+
+Menu();
+
+static void Menu()
+{
+    Console.WriteLine("Bem vindo ao Stopwatch!");
+    Console.WriteLine("");
+    Console.WriteLine("M = Minutos");
+    Console.WriteLine("S = Segundos");
+    Console.WriteLine("0 = Sair");
+    Console.WriteLine("Selecione a opção desejada:");
+}
+
+static void Start(int time)
+{
+
+    int currentTime = 0;
+
+    while (currentTime != time)
+    {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+
+    Console.Clear();
+    Console.WriteLine("Stopwatch finalizado");
+    Thread.Sleep(1500);
+    Console.WriteLine("Retornando para o menu...");
+    Thread.Sleep(2500);
+    Console.Clear();
+}
